6bf55c1292bf5383ccd76c9e3718a9782dc46e05
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-05-11 13:06:20 +0530

    drf quickstart

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 - [Django tutorial intro](https://docs.djangoproject.com/en/4.2/intro/)
     - Resume from: https://docs.djangoproject.com/en/4.2/intro/tutorial07/
     - Location: /your-first-django-app
-- [Django REST Framework](https://www.django-rest-framework.org/)
+- [Django REST Framework](https://www.django-rest-framework.org/tutorial/quickstart/)
+    - Resume from https://www.django-rest-framework.org/tutorial/1-serialization/
     - Location /django-rest-framework
--- django-rest-framework/firsdrf/urls.py
+++ django-rest-framework/firsdrf/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from firsdrf.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'usersss', views.UserViewSet)  # usersss shows which part of url this is
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    # path('api', include(router.urls)),  # TRYOUT
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
